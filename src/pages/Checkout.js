50cca44ea7ec8c379907dc68f750468e6ac9e215
shivang4857 <shivangd801@gmail.com>
2024-06-16 00:43:33 +0530

    checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,3 +1,10 @@
+
+import Cart from '../features/Cart/Cart';
+import React, { useState, Fragment } from 'react';
+
+
+
+
 import { Link } from 'react-router-dom';
 
 const products = [
@@ -315,64 +322,18 @@ function Checkout() {
           
           </form>
         </div>
+
+
         <div className="lg:col-span-2">
           <div className="mx-auto mt-12 bg-white max-w-7xl px-0 sm:px-0 lg:px-0">
             <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-              <h1 className="text-4xl my-5 font-bold tracking-tight text-gray-900">
-                Cart
-              </h1>
+              
               <div className="flow-root">
-                <ul role="list" className="-my-6 divide-y divide-gray-200">
-                  {products.map((product) => (
-                    <li key={product.id} className="flex py-6">
-                      <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                        <img
-                          src={product.imageSrc}
-                          alt={product.imageAlt}
-                          className="h-full w-full object-cover object-center"
-                        />
-                      </div>
 
-                      <div className="ml-4 flex flex-1 flex-col">
-                        <div>
-                          <div className="flex justify-between text-base font-medium text-gray-900">
-                            <h3>
-                              <a href={product.href}>{product.name}</a>
-                            </h3>
-                            <p className="ml-4">{product.price}</p>
-                          </div>
-                          <p className="mt-1 text-sm text-gray-500">
-                            {product.color}
-                          </p>
-                        </div>
-                        <div className="flex flex-1 items-end justify-between text-sm">
-                          <div className="text-gray-500">
-                            <label
-                              htmlFor="quantity"
-                              className="inline mr-5 text-sm font-medium leading-6 text-gray-900"
-                            >
-                              Qty
-                            </label>
-                            <select>
-                              <option value="1">1</option>
-                              <option value="2">2</option>
-                            </select>
+              <Cart></Cart>
+                           
                           </div>
 
-                          <div className="flex">
-                            <button
-                              type="button"
-                              className="font-medium text-indigo-600 hover:text-indigo-500"
-                            >
-                              Remove
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
             </div>
 
             <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
@@ -413,4 +374,9 @@ function Checkout() {
   );
 }
 
+
+
+
 export default Checkout;
+
+
